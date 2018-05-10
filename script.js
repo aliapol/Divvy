6a1b567f26d2e3ad3a918b3910ab942ebb99dc97
cameron <johnson.cameron.m@gmail.com>
2018-05-10 09:32:56 -0400

    shell of javascript with comments

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 "use strict"
+
+$(document).ready(() => {
+  // put all of the variables here
+      
+      
+  
+      $(document).on("click", ".card", (e) => {
+        //  when the cards get clicked this stuff happens
+          
+          
+          // when the button is clicked the clicked element adds the reserved class and removes the available class
+          // then 
+          button.click(() => {
+              
+          });
+      })
+
+      // when the aarow on the card is clicked the card will take up the whole screen to view.
+      .on("click", ".card_button", (e) => {
+          
+      })
+      // when the card on the summary page is clicked another div will appear that has an input for entering total budget
+      .on("click", "#total_view", (e) => {
+          #total_input
+      });
+      
+});
