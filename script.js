31f8646cf8c5ec75096e15d0d3a092b1e0d4799f
tabbatharenea@gmail.com <Renea415!>
2018-05-10 11:46:40 -0400

    working on expand card flick function and associated styles

--- script.js
+++ script.js
@@ -5,20 +5,31 @@ $(document).ready(() => {
     let total_budget ;  
     let expense_name ;
     let expense_amount;
-    
+    let defaultClass ="";
   
-      $(document).on("click", ".card", (e) => {
-        //  when the cards get clicked this stuff happens
-        $(e.target).addClass(".card_expand");
-        expense_amount = $("#expense_amount").val();
-        expense_name = $("#expense_name").val();
+      $("main").on("click", ".card", function(e) {
+        //  when the cards get default this stuff happens
+        
+        if($(this).hasClass("card_bills")){
+         defaultClass= "card_bills"; 
+        } else if ($(this).hasClass("card_food")){
+          defaultClass = "card_food";
+        } else if ($(this).hasClass("card_clothes")){
+            defaultClass = "card_clothes";
+        } else if ($(this).hasClass("card_spending")){
+          defaultClass = "card_clothes";
+        }    
+        console.log(defaultClass);
+        $(this).addClass(".card_expand").removeClass(defaultClass);
+        // expense_amount = $("#expense_amount").val();
+        // expense_name = $("#expense_name").val();
         
           
           // when the button is clicked the clicked element adds the reserved class and removes the available class
           // then 
-          button.click(() => {
-              $(e.target).removeClass(".card_expand");
-          });
+          // button.click(() => {
+          //     $(e.target).removeClass(".card_expand");
+          // });
       })
 
       // when the aarow on the card is clicked the card will take up the whole screen to view.
